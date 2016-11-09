e6a4460cec2c6a2d46f7d4e1421d9b56bfb30d48
Jayden <mischievousjayden@gmail.com>
2016-11-09 08:33:29 -0500

    added data sampling

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ClassifyingDriverBehavior
+=============================
+
+Tensorflow Installation
+-----------------------------
+- https://www.tensorflow.org/versions/r0.11/get_started/os_setup.html
+
+Tensorflow Tutorial
+-----------------------------
+- https://www.tensorflow.org/versions/r0.11/tutorials/index.html
+- https://github.com/aymericdamien/TensorFlow-Examples
+
---
+++ datasampling/datasampling.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+
+def sampleData(period, inputfile, outputfile):
+    """The function samples data. 
+    Example: sampleData(3, inputfile, outputfile)
+        create outputfile with every 3rd line in inputfile
+        
+    Args:
+        period (int): sampling period
+        inputfile (string): input file name
+        outputfile (string): output file name
+
+    Returns:
+        void
+    """
+    fread = open(inputfile, 'r')
+    fwrite = open(outputfile, 'w')
+
+    with open(inputfile, 'r') as fread, open(outputfile, 'w') as fwrite:
+        for i, line in enumerate(fread):
+            if (i % period) == 0:
+                fwrite.write(line)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        message = "Usag: datafilter.py <period> <input_file> <output_file>"
+        sys.exit(message)
+
+    period = int(sys.argv[1])
+    inputfile = sys.argv[2]
+    outputfile = sys.argv[3]
+    sampleData(period, inputfile, outputfile)
+
