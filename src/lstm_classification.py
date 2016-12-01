d036c37cab7d71baab0f733653db154e06639a57
Jayden <mischievousjayden@gmail.com>
2016-11-30 22:47:42 -0500

    applied cross validation on lstm

--- src/lstm_classification.py
+++ src/lstm_classification.py
@@ -5,62 +5,45 @@ import numpy as np
 
 import datalayer as dl
 
+import pdb
 
 # read data
 print("read data")
 data_path = "../data"
-data = dl.driverdata(data_path) # data.expert_data, data.inexpert_data
+data = dl.driverdata(data_path) # data.input_data, data.getCrossValidationInput(num_groups, group)
 
-# for d in data.expert_data:
-#     print(np.shape(d))
-#
-# for d in data.inexpert_data:
-#     print(np.shape(d))
+# print("length " + str(len(data.input_data)))
+# print("length " + str(len(data.input_data["expert"])))
+# print("length " + str(len(data.input_data["inexpert"])))
 
 
 # Parameters
+n_cross_validation = 4
 learning_rate = 0.001
-training_iters = 100
-batch_size = 6
+training_iters = 1000
+batch_size = 12
 display_step = 10
 
 # Network Parameters
-n_input = 101 # MNIST data input (img shape: 28*28)
+n_input = 23 # the number of filtered column # 101 # the number of column
 n_steps = 100 # 2629 # timesteps # 500 #
-n_hidden = 256 # hidden layer num of features
+n_hidden = 128 # 128 # hidden layer num of features
 n_classes = 2 # MNIST total classes (0-9 digits)
 
 
+def cutData(data, num_row):
+    features = []
+    label = []
+    for d in data:
+        # features.append(np.array(d["features"])[:n_steps,:])
+        features.append(d["features"][:n_steps,:])
+        label.append(d["label"])
+    return [np.array(features), np.array(label)]
 
-print("build train and test data")
-def createTrainTestData(expert_data, inexpert_data):
-    train_data = []
-    train_label = []
-    test_data = []
-    test_label = []
-
-    for i in range(4):
-        if(i != 3):
-            train_data.append(np.array(expert_data[i])[:n_steps,:])
-            train_label.append([1,0])
-        else:
-            test_data.append(np.array(expert_data[i])[:n_steps,:])
-            test_label.append([1,0])
-
-    for i in range(4):
-        if(i != 3):
-            train_data.append(np.array(inexpert_data[i])[:n_steps,:])
-            train_label.append([0,1])
-        else:
-            test_data.append(np.array(inexpert_data[i])[:n_steps,:])
-            test_label.append([0,1])
-
-    return [train_data, train_label, test_data, test_label]
-
-train_data, train_label, test_data, test_label = createTrainTestData(data.expert_data, data.inexpert_data)
-
-
-
+def createTrainTestData(cross_validation_data):
+    train_features, train_label = cutData(cross_validation_data["train"], n_steps)
+    test_features, test_label = cutData(cross_validation_data["test"], n_steps)
+    return [train_features, train_label, test_features, test_label]
 
 
 
@@ -100,6 +83,7 @@ def RNN(x, weights, biases):
     # Linear activation, using rnn inner loop last output
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
+
 pred = RNN(x, weights, biases)
 
 # Define loss and optimizer
@@ -115,29 +99,33 @@ init = tf.initialize_all_variables()
 
 # Launch the graph
 with tf.Session() as sess:
-    print("start learning")
-    sess.run(init)
-    step = 1
-    # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        # print("step " + str(step))
-        batch_x = train_data
-        batch_y = train_label
+    for i in range(n_cross_validation):
+        print("build train and test data")
+        train_data, train_label, test_data, test_label = createTrainTestData(data.getCrossValidationInput(n_cross_validation, i))
 
-        # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-        if step % display_step == 0:
-        # Calculate batch accuracy
-            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-            # Calculate batch loss
-            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
-            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.5f}".format(acc))
-        step += 1
-    print("Optimization Finished!")
+        print("start learning")
+        sess.run(init)
+        step = 1
+        # Keep training until reach max iterations
+        while step * batch_size < training_iters:
+            # print("step " + str(step))
+            batch_x = train_data
+            batch_y = train_label
 
-    # Calculate accuracy for 128 mnist test images
-    print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
+            # Run optimization op (backprop)
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+            if step % display_step == 0:
+            # Calculate batch accuracy
+                acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+                # Calculate batch loss
+                loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+                print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.5f}".format(acc))
+            step += 1
+        print("Optimization Finished!")
+
+        # Calculate accuracy for 128 mnist test images
+        print("Testing Accuracy:", \
+                sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
 
