626d1e2cc8517450329416bf61e8f9a377b04d5a
Jayden <mischievousjayden@gmail.com>
2016-11-30 19:46:06 -0500

    updated datalayer for cross validation

--- src/datalayer.py
+++ src/datalayer.py
@@ -19,26 +19,65 @@ class driverdata(object):
                 'inexpert' directory contains inexpert driving data
             suffix: data file extension. default is ".dat"
         """
-        self.expert_data = self.createData(datapath + "/expert", suffix)
-        self.inexpert_data = self.createData(datapath + "/inexpert", suffix)
+        self.input_data = {
+            "expert": self.createData(datapath + "/expert", suffix, 0),
+            "inexpert": self.createData(datapath + "/inexpert", suffix, 1)
+        }
 
-    def createData(self, path, suffix):
-        """ createData
+    def getCrossValidationInput(self, num_groups, group):
+        """getCrossValidationInpu
+        Args:
+            num_groups: seperate entire data to num_groups
+            group: group number. Start from 0 to num)groups-1
+        Return:
+            dictionary with train and test data
+        """
+        train_data = []
+        test_data = []
+        for i in range(num_groups):
+            for key in self.input_data:
+                if(i != group):
+                    train_data = np.append(train_data, self.input_data[key][i::num_groups])
+                else:
+                    test_data = np.append(test_data, self.input_data[key][i::num_groups])
+        train_data = np.array(np.random.permutation(train_data))
+        test_data = np.array(np.random.permutation(test_data))
+        return {"train":train_data, "test":test_data}
+
+    def createData(self, path, suffix, label):
+        """createData
         Args:
             path: directory path
             suffix: data file suffix
-        Returns:
-            void
+            label: 0 => [0, 1], 1 => [1, 0]
+        Return:
+            list suffled data with label
+            example_data_structure)
+            [dictionary{features:data, label:label}, dictionary{features:data, label:label}, ...]
+        """
+        data_set = self.readData(path, suffix)
+        data_set = self.parseData(data_set)
+        data_set = self.addLabel(data_set, label)
+        data_set = np.random.permutation(data_set)
+        return data_set
+
+    def readData(self, path, suffix):
+        """ readData
+        Args:
+            path: directory path
+            suffix: data file suffix
+        Return:
+            data set
         """
         files = fu.getAllFileNames(path, suffix)
-        return self.modifyData(np.array(fu.readDataFiles(files, " ")))
+        return np.array(fu.readDataFiles(files, " "))
 
-    def modifyData(self, data_set):
-        """modifyData
+    def parseData(self, data_set):
+        """parseData
         skip header and convert string value to float
         Args:
             data_set: three dimensional numpy array
-        Returns:
+        Return:
             modified data_set without header
         """
         result = []
@@ -51,5 +90,23 @@ class driverdata(object):
 
             # convert string to float
             result.append(data[1:,:].astype(np.float))
-        return result
+        return np.array(result)
+
+    def addLabel(self, data_set, label):
+        """ addLabel
+        add label on data
+        Args:
+            data_set: three dimensional numpy array
+            label: 0 => [0, 1], 1 => [1, 0]
+        Return:
+            data_set with label
+        """
+        result = []
+        for data in data_set:
+            data_dictionary = {
+                "features": data,
+                "label": [0,1] if label == 0 else [1,0]
+            }
+            result.append(data_dictionary)
+        return np.array(result)
 
