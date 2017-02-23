a4cf150440f7f930ce9e6c57b6b2a84627b3719d
Santiago Ontanon <santi@n3-74-241.dhcp.drexel.edu>
2017-02-23 13:06:32 -0500

    experimented with bar plot

--- paper/graph/graph.py
+++ paper/graph/graph.py
@@ -5,9 +5,12 @@ num_neurons = ["16", "32", "64", "128", "256"]
 raw_accuracy = [0.5, 0.5, 0.6875, 0.59375, 0.5]
 avg_accuracy = [0.75, 0.5, 0.25, 1.0, 0.5]
 
-width = 0.6
-color = "blue"
-plt.bar(range(len(raw_accuracy)), raw_accuracy, width=width, color=color)
+ind = np.arange(len(raw_accuracy));
+width = 0.3
+color1 = "blue"
+plt.bar(ind-width/2, raw_accuracy, width=width, color=color1)
+color2 = "red"
+plt.bar(ind+width/2, avg_accuracy, width=width, color=color2)
 plt.xticks(range(len(raw_accuracy)), num_neurons)
 plt.title("Accuracy vs. The number of Neurons")
 plt.xlabel("Accuracy")
